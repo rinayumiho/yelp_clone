c3109fdb359abd929637add0a70d4ca7a47189da
gamestar <rinayumiho@gmail.com>
2021-09-08 17:42:29 -0400

    Prepare for initial Heroku push

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -17,7 +17,6 @@ class SessionForm extends React.Component {
     }
 
     handleSubmit(e) {
-        debugger
         e.preventDefault();
         const user = Object.assign({}, this.state);
         this.props.processForm(user);
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -7,10 +7,8 @@ const _nullUser = Object.freeze({
   
 const sessionReducer = (oldState = _nullUser, action) => {
     Object.freeze(oldState);
-    debugger
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
-            debugger
             return { id: action.user };
         case LOGOUT_CURRENT_USER:
             return _nullUser;
