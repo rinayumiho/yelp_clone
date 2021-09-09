b6885f3f1f03e6142acd17ed12cadc5411128e55
gamestar <rinayumiho@gmail.com>
2021-09-09 11:05:29 -0400

    splash background

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
 const path = require('path');
-const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-const server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
 
 module.exports = {
   context: __dirname,
