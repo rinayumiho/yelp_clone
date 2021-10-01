567c998a27fb5a713a4db3a7787fee99f22a7fbf
gamestar <rinayumiho@gmail.com>
2021-10-01 00:04:36 -0400

    Add angellist link

--- frontend/components/splash_about/splash_about.jsx
+++ frontend/components/splash_about/splash_about.jsx
@@ -13,6 +13,10 @@ const SplashAbout = props => (
                     
                     <a href="https://www.linkedin.com/in/long-chen-5153a31b7/" target="_blank"><img className="about-pic" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn"/></a>
                 </div>
+                <div className="about-link">
+                    
+                    <a href="https://angel.co/u/long-chen-14" target="_blank"><img className="about-pic" src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/angellist-512.png" alt="AngelList"/></a>
+                </div>
             </div>  
         </div>
         <img className="bottom-image"src="http://d5yem10y0aabn.cloudfront.net/yelp_footer.png" alt="bottom-pic"/>
